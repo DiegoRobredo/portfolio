399df27e79763e7b54452dc57e628764829ebe61
Diego Robredo Del Rey <diego.robredo@reworldtechnologies.com>
2024-08-25 19:44:12 +0200

    Montaje de pildoras

---
+++ src/utils/constants.js
@@ -0,0 +1,45 @@
+export const EXPERIENCE = [
+  {
+    date: "Enero 2023",
+    title: "CTO y Cofundador",
+    company: "Reworld Technologies",
+    description:
+      "Desarrollo y gestión de productos. Supervisión del ciclo de vida completo del desarrollo de productos. Desarrollo de una infraestructura escalable y robusta en AWS. Liderazgo en innovación fomentando una cultura de creatividad y mejora tecnológica continua.",
+    link: "#"
+  },
+  {
+    date: "Septiembre 2016",
+    title: "Senior Consultant",
+    company: "Enzyme Advising Group",
+    description:
+      "Arquitecto y desarrollador de aplicaciones web y mobile en entornos cloud y onpremise. Enfocado principalmente en el desarrollo frontend en múltiples frameworks basados en javascript y al desarrollo backend en Node.js y Python."
+  }
+];
+
+export const NAV_ITEMS = [
+  {
+    title: "Home",
+    label: "home",
+    url: "/#"
+  },
+  {
+    title: "Experiencia",
+    label: "experiencia",
+    url: "/#experiencia"
+  },
+  {
+    title: "Proyectos",
+    label: "proyectos",
+    url: "/#proyectos"
+  },
+  {
+    title: "Sobre mí",
+    label: "sobre-mi",
+    url: "/#sobre-mi"
+  },
+  {
+    title: "Contacto",
+    label: "contacto",
+    url: "/#contacto"
+  }
+];
