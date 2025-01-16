b2b4427c7573428bf30496aa2409a263ba29fe8f
Diego Robredo <drobredo@gmail.com>
2025-01-16 20:44:51 +0100

    Section container and constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,17 +1,33 @@
 export const EXPERIENCE = [
   {
-    date: "Enero 2023",
-    title: "CTO y Cofundador",
-    company: "Reworld Technologies",
-    description:
-      "Desarrollo y gestión de productos. Supervisión del ciclo de vida completo del desarrollo de productos. Desarrollo de una infraestructura escalable y robusta en AWS. Liderazgo en innovación fomentando una cultura de creatividad y mejora tecnológica continua."
+    from: "April 2024",
+    to: "Present",
+    company: "Stratesys S.L.",
+    company_info:
+      "Consulting firm specializing in transforming business operations and processes through emerging technologies",
+    job: "Lead Developer",
+    job_description:
+      "Web and mobile application architect and developer for cloud and on-premise environments. Specialized in frontend development using various JavaScript frameworks and backend development with Node.js and Python"
   },
   {
-    date: "Septiembre 2016",
-    title: "Senior Consultant",
+    from: "January 2023",
+    to: "Present",
+    job: "CTO y Cofundador",
+    company: "Reworld Technologies",
+    company_info:
+      "Aims to revolutionize how people engage with their surroundings by creating a platform to discover and share unique experiences globally",
+    job_description:
+      "Product development and management. Oversight of the full product lifecycle. Built scalable, robust infrastructure on AWS. Led innovation by fostering a culture of creativity and continuous technological improvement"
+  },
+  {
+    from: "September 2016",
+    to: "April 2024",
+    job: "Senior Consultant",
     company: "Enzyme Advising Group",
-    description:
-      "Arquitecto y desarrollador de aplicaciones web y mobile en entornos cloud y onpremise. Enfocado principalmente en el desarrollo frontend en múltiples frameworks basados en javascript y al desarrollo backend en Node.js y Python."
+    company_info:
+      "Tech consulting firm specializing in transforming business operations through emerging technologies",
+    job_description:
+      "Web and mobile application architect and developer for cloud and on-premise SAP environments. Focused on frontend development with JavaScript frameworks and backend with Node.js and Python"
   }
 ];
 
